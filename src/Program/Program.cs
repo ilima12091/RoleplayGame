ed21e5a1603c77715b2eaeb512e1868e1a75230e
Ignacio Lima <limaignacio12091@gmail.com>
2020-09-18 16:41:39 -0300

    agrego la creacion de los personajes

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
-namespace Program
+namespace RoleplayGame
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Dwarf dwarf = new Dwarf("Rodri", 10, 10, 10, "El enano Rodri");
+            Elf elf = new Elf("Jero", 10, 10, 30, 10, "El elfo Jero");
+            Wizard wizard = new Wizard("Nacho", 250, 10, 10, 10, "El mago Nacho");
+            Console.WriteLine("Rodri: " + dwarf.CurrentLife + "\n" + "Jero: " + elf.CurrentLife + "\n" + "Nacho: " + wizard.CurrentLife);
+            elf.AttackWizard(wizard);
+            Console.WriteLine("\n" + "Rodri: " + dwarf.CurrentLife + "\n" + "Jero: " + elf.CurrentLife + "\n" + "Nacho: " + wizard.CurrentLife);
         }
     }
 }
